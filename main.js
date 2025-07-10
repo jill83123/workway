e474371fe9060dda07bd1d533a7ffc336287d78d
YU-FANG <jill83123@gmail.com>
2025-07-10 17:05:58 +0800

    🎨 style: 依規範初步設定樣式

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+document.querySelectorAll('.dropdown[data-select="true"]').forEach((dropdown) => {
+  const btn = dropdown.querySelector('.dropdown-toggle');
+  const menu = dropdown.querySelector('.dropdown-menu');
+  const menuItems = menu.querySelectorAll('.dropdown-item');
+
+  menuItems.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (btn.textContent !== item.textContent) {
+        btn.textContent = item.textContent;
+      }
+    });
+  });
+});
